869cc567c67f6c0d50b88be4c54dad89a2c441e4
pixcelo <bodomu@gmail.com>
2022-11-25 05:28:31 +0900

    add: オーダー時に使用するモデル

---
+++ Models/Condition.cs
@@ -0,0 +1,8 @@
+namespace Zaku
+{
+    public class Condition
+    {
+        public bool IsOk { get; set; }
+        public Side Side { get; set; }
+    }
+}
--- Models/Order.cs
+++ Models/Order.cs
@@ -5,9 +5,9 @@ namespace Zaku
         public string? Symbol { get; set; }
         public Side OrderSide { get; set; }
         public decimal EntryPrice { get; set; }
-        public decimal SettlementPrice { get; set; }
+        public decimal? SettlementPrice { get; set; }
         public decimal Lots { get; set; }
-        public decimal TakeProfit { get; set; }
-        public decimal StopLoss { get; set; }
+        public decimal? TakeProfit { get; set; }
+        public decimal? StopLoss { get; set; }
     }
 }
