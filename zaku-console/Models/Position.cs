ec7a850bf511628221b223a8aaf8b4280d771348
pixcelo <bodomu@gmail.com>
2022-12-09 04:32:38 +0900

    mod:ConditionクラスをPositionクラスに内包

--- zaku-console/Models/Position.cs
+++ zaku-console/Models/Position.cs
@@ -8,9 +8,19 @@ namespace Zaku
         public OrderType Type { get; set; }
         public OrderSide Side { get; set; }
         public decimal EntryPrice { get; set; }
-        public decimal? SettlementPrice { get; set; }
+        public decimal? ClosePrice { get; set; }
         public decimal Lots { get; set; }
         public decimal? TakeProfit { get; set; }
         public decimal? StopLoss { get; set; }
+        public decimal Profit { get; set; }
+        public decimal Fee { get; set; }
+        public bool EntryCondition { get; set; }
+        public bool CloseCondition { get; set; }
+
+        public Position()
+        {
+            this.EntryCondition = false;
+            this.CloseCondition = false;
+        }
     }
 }
