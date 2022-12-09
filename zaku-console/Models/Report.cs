6d7becbb6cba41a3359107d5f24f0fb56642c8fc
pixcelo <bodomu@gmail.com>
2022-12-10 04:16:52 +0900

    mod: プロパティ追加

--- zaku-console/Models/Report.cs
+++ zaku-console/Models/Report.cs
@@ -3,9 +3,16 @@ namespace Zaku
     public class Report
     {
         public decimal Profit { get; set; }
+        public decimal Fee { get; set; }
         public decimal Loss { get; set; }
         public decimal RateWin { get; set; }
         public decimal RateLose { get; set; }
         public decimal PaidCommission { get; set; }
+        public List<Position> List { get; set; }
+
+        public Report()
+        {
+            this.List = new List<Position>();
+        }
     }
 }
