6e40711bff3954e230fef99a8c7fd8dccde06383
pixcelo <bodomu@gmail.com>
2022-12-11 16:52:58 +0900

    mod: モデルクラス修正

--- zaku-console/Models/Report.cs
+++ zaku-console/Models/Report.cs
@@ -3,11 +3,12 @@ namespace Zaku
     public class Report
     {
         public decimal Profit { get; set; }
-        public decimal Fee { get; set; }
         public decimal Loss { get; set; }
+        public decimal Fee { get; set; }
         public decimal RateWin { get; set; }
         public decimal RateLose { get; set; }
-        public decimal PaidCommission { get; set; }
+        public decimal MaxDrawdown { get; set; }
+        public int NumberOfTrading { get; set; }
         public List<Position> List { get; set; }
 
         public Report()
